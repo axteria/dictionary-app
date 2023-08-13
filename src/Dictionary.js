3ef767cd5ab027664f987ff855a79bf9036be1b1
axteria <fayee.wu@hotmail.com>
2023-08-13 16:15:46 +1000

    adding pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,13 +8,23 @@ export default function Dictionary(props) {
   let [results, SetResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     SetResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "0IjYhJs2LcTYhUsk2LbbrjzmQ0UvQeoVkEP1YWV3hmXtLxNpHsbU4Ylm";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
