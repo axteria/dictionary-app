b0360148b25056835a6f6f37838e6c1b969cdaad
axteria <fayee.wu@hotmail.com>
2023-08-14 20:47:14 +1000

    fix netlify bug not fetching

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./Phonetics.css"
+import "./Phonetics.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <span className="text">{props.phonetic.text}</span>
-      <a href={props.phonetic.audio} target="_blank">
+      <a rel="noreferrer" href={props.phonetic.audio} target="_blank">
         🔊 Listen
       </a>
     </div>
